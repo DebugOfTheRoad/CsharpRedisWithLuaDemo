2e06dcfbf6e3912ce628bef4ae91e4da96103fe1
debug <153950875@qq.com>
2016-05-22 21:21:03 +0800

    aaaa

---
+++ src/CSharpRedisLuaDemo/CSharpRedisLuaDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpRedisLuaDemo
+{
+    class Program
+    {
+        static RedisClient redisClient = new RedisClient("localhost", 6379);//redis服务IP和端口
+        static void Main(string[] args)
+        {
+            Console.WriteLine(redisClient.Get<string>("city")); 
+            Console.ReadKey();
+        }
+    }
+ 
+}
